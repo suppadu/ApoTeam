c7dcd53f5213ff0a31a8f8843c6f6dc0d95a6ed2
konstantinov_d <konstantinov.d@orionnet.ru>
2021-12-01 16:50:03 +0800

    Initial Commit

---
+++ ApoTeam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ApoTeam
+//
+//  Created by Дмитрий Константинов on 01.12.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
